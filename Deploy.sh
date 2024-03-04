d424dbdd15bd5b874b1eaa78b129a3a3ef80686e
Luis Andrey Zuñiga Hernández <luisandreyzuniga@gmail.com>
2024-03-04 13:17:37 -0600

    Updating deploy
    The system could read the OPENAI_API_KEY variable

--- Deploy.sh
+++ Deploy.sh
@@ -2,9 +2,9 @@ echo "Please input your OPENAI_API_KEY: "
 read OPENAI_API_KEY
 chmod 400 VM_KEY/Recommendation_Key.pem
 scp -i VM_KEY/Recommendation_Key.pem -rp *  ubuntu@ec2-34-208-253-148.us-west-2.compute.amazonaws.com:SOA
-ssh -i VM_KEY/Recommendation_Key.pem ubuntu@ec2-34-208-253-148.us-west-2.compute.amazonaws.com '
-export PATH="$PATH:/home/ubuntu/.local/bin"
-export OPENAI_API_KEY=$OPENAI_API_KEY
+ssh -i VM_KEY/Recommendation_Key.pem ubuntu@ec2-34-208-253-148.us-west-2.compute.amazonaws.com "
+export PATH=\"\$PATH:/home/ubuntu/.local/bin\"
+export OPENAI_API_KEY=\"$OPENAI_API_KEY\"
 sudo apt-get update
 sudo apt install -y python3-pip nginx
 pip install fastapi uvicorn python-dotenv openai
@@ -13,4 +13,4 @@ sudo cp recommendation_nginx /etc/nginx/sites-enabled/
 sudo service nginx restart
 cd app
 uvicorn main:app --reload
-'
+"
